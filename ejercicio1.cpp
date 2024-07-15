4abb680c88efea2d2967e1969bc23bdfe51a314d
cmmamaniqu <cmmamaniqu@unjbg.edu.pe>
2024-07-15 12:14:48 -0500

    se sube el ejercicio1

---
+++ ejercicio1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+     int n;
+
+     cout<<"Ingrese el orden de la matriz: "; cin>>n;
+     int M[n][n];
+   
+
+     for(int i=0; i<n; i++){
+       
+     for(int j=0; j<n; j++){
+        cout<<"Digite un numero ["<<i<<"]["<<j<<"]";
+       
+         cin>>M[i][j];
+       }
+     }
+     int s=0;
+    for( int i=0; i<n; i++){
+     for(int j=i+1; j<n; j++){
+        s=s + M[i][j];
+      
+       }
+    }
+ cout<<"La suma de la diagonal superior de la matriz: "<<s;
+      return 0;
+     }
